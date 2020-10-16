df027458bc0b52b233fb8eb1c49ecbdc8366ff22
Oliver Müske <info@oliver-mueske.de>
2020-10-16 08:49:26 +0200

    Schema Locations with timestamps extended

--- src/models/Location.js
+++ src/models/Location.js
@@ -1,13 +1,16 @@
 // External Dependencies
 const mongoose = require("mongoose");
 
-const locationSchema = new mongoose.Schema({
-  name: String,
-  adress: String,
-  longitude: String,
-  latitude: String,
-  hives: Array,
-  comment: String,
-});
+const locationSchema = new mongoose.Schema(
+  {
+    name: String,
+    adress: String,
+    longitude: String,
+    latitude: String,
+    hives: Array,
+    comment: String,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Location", locationSchema);
