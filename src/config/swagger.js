bc4175e0282c9af58f530e843bf828ca3690f287
Oliver Müske <info@oliver-mueske.de>
2020-11-28 11:24:46 +0100

    Swagger Configuration updated

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,19 +1,27 @@
 exports.options = {
-  routePrefix: "/documentation",
+  routePrefix: '/documentation',
   exposeRoute: true,
   swagger: {
     info: {
-      title: "BeeBackend API",
-      description: "BeeBackend API Services",
-      version: "1.0.0",
+      title: 'BeeBackend API',
+      description: 'BeeBackend API Services',
+      version: '1.0.0'
     },
     externalDocs: {
-      url: "https://swagger.io",
-      description: "Find more info here",
+      url: 'https://swagger.io',
+      description: 'Find more info here'
     },
-    host: "localhost",
-    schemes: ["http"],
-    consumes: ["application/json"],
-    produces: ["application/json"],
-  },
-};
+    host: 'localhost',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    tags: [
+      {
+        name: 'configurations',
+        description: 'Frontend related Configuration end-points'
+      },
+      { name: 'beeHives', description: 'BeeHives related end-points' },
+      { name: 'locations', description: 'Locations related end-points' }
+    ]
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -13,11 +13,17 @@ const routes = [
   */
   {
     method: 'GET',
+    schema: {
+      tags: ['configurations']
+    },
     url: '/api/configurations',
     handler: configurationController.getConfigurations
   },
   {
     method: 'GET',
+    schema: {
+      tags: ['configurations']
+    },
     url: '/api/configurations/:id',
     handler: configurationController.getSingleConfiguration
   },
@@ -28,38 +34,58 @@ const routes = [
   */
   {
     method: 'GET',
+    schema: {
+      tags: ['locations']
+    },
     url: '/api/locations',
     handler: locationController.getLocations
   },
   {
     method: 'GET',
+    schema: {
+      tags: ['locations']
+    },
     url: '/api/locations/:id',
     handler: locationController.getSingleLocation
   },
   {
     method: 'POST',
     url: '/api/locations',
-    handler: locationController.addLocation,
-    schema: { name: String }
+    schema: {
+      tags: ['locations']
+    },
+    handler: locationController.addLocation
   },
   {
     method: 'PUT',
     url: '/api/locations/:id',
+    schema: {
+      tags: ['locations']
+    },
     handler: locationController.updateLocation
   },
   {
     method: 'PUT',
     url: '/api/locations/:id/link/:beeHiveID',
+    schema: {
+      tags: ['locations']
+    },
     handler: locationController.linkBeeHiveToLocation
   },
   {
     method: 'PUT',
     url: '/api/locations/:id/unlink/:beeHiveID',
+    schema: {
+      tags: ['locations']
+    },
     handler: locationController.unlinkBeeHiveFromLocation
   },
   {
     method: 'DELETE',
     url: '/api/locations/:id',
+    schema: {
+      tags: ['locations']
+    },
     handler: locationController.deleteLocation
   },
 
@@ -71,57 +97,89 @@ const routes = [
   {
     method: 'GET',
     url: '/api/beeHives',
+    schema: {
+      tags: ['beeHives']
+    },
     handler: beeHiveController.getBeeHives
   },
   {
     method: 'GET',
     url: '/api/beeHives/:id',
+    schema: {
+      tags: ['beeHives']
+    },
     handler: beeHiveController.getSingleBeeHive
   },
   {
     method: 'POST',
     url: '/api/beeHives',
-    handler: beeHiveController.addBeeHive,
-    schema: { name: String }
+    schema: {
+      tags: ['beeHives']
+    },
+    handler: beeHiveController.addBeeHive
   },
   {
     method: 'PUT',
     url: '/api/beeHives/:id',
+    schema: {
+      tags: ['beeHives']
+    },
     handler: beeHiveController.updateBeeHive
   },
   {
     method: 'DELETE',
     url: '/api/beeHives/:id',
+    schema: {
+      tags: ['beeHives']
+    },
     handler: beeHiveController.deleteBeeHive
   },
   {
     method: 'GET',
     url: '/api/beeHives/logs',
+    schema: {
+      tags: ['beeHives']
+    },
     handler: beeHiveController.getAllBeeHiveLogs
   },
   {
     method: 'GET',
     url: '/api/beeHives/:id/logs',
+    schema: {
+      tags: ['beeHives']
+    },
     handler: beeHiveController.getBeeHiveLogs
   },
   {
     method: 'GET',
     url: '/api/beeHives/logs/:id',
+    schema: {
+      tags: ['beeHives']
+    },
     handler: beeHiveController.getSingleBeeHiveLog
   },
   {
     method: 'POST',
     url: '/api/beeHives/:id/logs',
+    schema: {
+      tags: ['beeHives']
+    },
     handler: beeHiveController.addBeeHiveLog
   },
   {
     method: 'DELETE',
     url: '/api/beeHives/logs/:id',
+    schema: {
+      tags: ['beeHives']
+    },
     handler: beeHiveController.deleteBeeHiveLog
   },
   {
     method: 'PUT',
     url: '/api/beeHives/logs/:id',
+    schema: {
+      tags: ['beeHives']
+    },
     handler: beeHiveController.updateBeeHiveLog
   }
 ]
